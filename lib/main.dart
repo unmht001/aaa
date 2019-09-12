2a4593ff891525e638ced0ca76afb11cc92f0585
unmht001 <654308402@qq.com>
2019-09-12 17:41:20 +0800

    19.09.12-解决了返回键的小问题

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:aaa/init_fun.dart';
 import 'package:aaa/page2.dart';
 import 'package:aaa/page3.dart';
 import 'package:aaa/page4.dart';
-import 'package:aaa/pck/get_string.dart';
+// import 'package:aaa/pck/get_string.dart';
 import 'package:aaa/pck/tts_helper.dart';
 import 'package:aaa/shujia.dart';
 import 'package:flutter/material.dart';
@@ -58,7 +58,72 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   int _cindex = 0;
   TabController _controller;
-  final PageController _pctler = new PageController();
+  final PageController _pctler = new PageController(initialPage: 0);
+  DateTime _lastPressAt;
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () async {
+          if (_lastPressAt == null ||
+              _pctler.page > _pctler.initialPage ||
+              DateTime.now().difference(_lastPressAt) > Duration(seconds: 1)) {
+            _lastPressAt = DateTime.now();
+            _pctler.previousPage(duration: Duration(milliseconds: 300), curve: Curves.ease);
+            return false;
+          }
+          return true;
+        },
+        child: PageView(controller: _pctler, children: <Widget>[
+          Scaffold(
+              //page 1
+              body: TabBarView(
+                  controller: _controller,
+                  children: <Widget>[Shujia(itemonpress: onShujiaPress), Page2(), Page3(sdkdata: sdkdata), Page4()]),
+              bottomNavigationBar: BottomNavigationBar(
+                  type: BottomNavigationBarType.fixed,
+                  iconSize: 24.0,
+                  currentIndex: _cindex,
+                  fixedColor: Colors.red,
+                  items: navs(),
+                  onTap: (int x) => setState(() {
+                        _cindex = x;
+                        _controller.index = x;
+                      }))),
+          Scaffold(
+              //page 2
+              body: MenuPage(itemonpress: onMenuPress)),
+          Scaffold(
+              //page 3
+              body: ContentPage(pageReadOverAction: onPagePress))
+        ]));
+  }
+
+  List<BottomNavigationBarItem> navs() {
+    List<BottomNavigationBarItem> _r = [];
+    for (var x in (ListenerBox.instance['navs'].inited ? ListenerBox.instance['navs'].value : []))
+      _r.add(BottomNavigationBarItem(title: Text(x.tt), icon: Icon(x.icon)));
+    return _r;
+  }
+
+  @override
+  void dispose() {
+    this._controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    this._controller = TabController(
+        vsync: this, length: ListenerBox.instance['navs'].inited ? ListenerBox.instance['navs'].value.length : 0);
+    super.initState();
+  }
+
+  void openpage(BookData bk, {int page: 1}) {
+    bk.menuLsn.afterSetter = () => setState(() {});
+    // PageOp.getmenudata(bk);
+    this._pctler.animateToPage(page, duration: Duration(milliseconds: 300), curve: Curves.ease);
+  }
 
   onShujiaPress(BookData bk) => setState(() {
         this.widget.refresh();
@@ -87,58 +152,5 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
       ListenerBox.instance['bk'].value.selected = ListenerBox.instance['bk'].value.selected - 1;
       (ListenerBox.instance['bk'].value as BookData).getpagedata();
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return PageView(controller: _pctler, children: <Widget>[
-      Scaffold(
-          //page 1
-          body: TabBarView(
-              controller: _controller,
-              children: <Widget>[Shujia(itemonpress: onShujiaPress), Page2(), Page3(sdkdata: sdkdata), Page4()]),
-          bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              iconSize: 24.0,
-              currentIndex: _cindex,
-              fixedColor: Colors.red,
-              items: navs(),
-              onTap: (int x) => setState(() {
-                    _cindex = x;
-                    _controller.index = x;
-                  }))),
-      Scaffold(
-          //page 2
-          body: MenuPage(itemonpress: onMenuPress)),
-      Scaffold(
-          //page 3
-          body: ContentPage(pageReadOverAction: onPagePress))
-    ]);
-  }
-
-  List<BottomNavigationBarItem> navs() {
-    List<BottomNavigationBarItem> _r = [];
-    for (var x in (ListenerBox.instance['navs'].inited ? ListenerBox.instance['navs'].value : []))
-      _r.add(BottomNavigationBarItem(title: Text(x.tt), icon: Icon(x.icon)));
-    return _r;
-  }
-
-  @override
-  void dispose() {
-    this._controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    this._controller = TabController(
-        vsync: this, length: ListenerBox.instance['navs'].inited ? ListenerBox.instance['navs'].value.length : 0);
-    super.initState();
-  }
-
-  void openpage(BookData bk, {int page: 1}) {
-    bk.menuLsn.afterSetter = () => setState(() {});
-    // PageOp.getmenudata(bk);
-    this._pctler.animateToPage(page, duration: Duration(milliseconds: 300), curve: Curves.ease);
   }
 }
